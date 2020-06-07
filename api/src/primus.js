a2d05d3cbea9134da6a1912b94f15552f3bfbd08
Florian Pichler <fp@ylk.gd>
2020-06-07 16:23:31 +0200

    Better syntax for primus events

--- api/src/primus.js
+++ api/src/primus.js
@@ -1,6 +1,14 @@
 const Primus = require('primus');
 const rooms = require('primus-rooms');
 
+const onData = (spark, type, callback) => {
+  return spark.on('data', async (payload) => {
+    if (payload.type === type) {
+      return callback(payload);
+    }
+  });
+};
+
 module.exports = (server, store, options) => {
   const primus = new Primus(server, {
     iknowhttpsisbetter: true,
@@ -14,53 +22,38 @@ module.exports = (server, store, options) => {
   primus.on('connection', async (spark) => {
     let room;
 
-    spark.on('data', async ({ type, id, ...data }) => {
-      if (type === 'room.join') {
-        spark.join(data.room, () => {
-          spark.write({
-            type,
-            id,
-            ...data,
-          });
-        });
+    onData(spark, 'room.join', (payload) => {
+      spark.join(payload.room, () => {
+        spark.write(payload);
+      });
 
-        room = data.room;
+      room = payload.room;
+    });
 
-        return;
-      }
-
+    onData(spark, 'room.sync', async ({ type, id, ...data }) => {
       if (!room) {
         return;
       }
 
-      if (type === 'room.sync') {
-        if (Object.keys(data).length > 0) {
-          await store.setRoom(room, data);
-        }
+      if (Object.keys(data).length > 0) {
+        await store.setRoom(room, data);
+      }
 
-        const roomData = await store.getRoom(room);
+      const roomData = await store.getRoom(room);
 
-        spark.room(room).write({
-          type,
-          id,
-          ...roomData,
-        });
+      spark.room(room).write({
+        type,
+        id,
+        ...roomData,
+      });
+    });
 
+    onData(spark, 'room.sound', (payload) => {
+      if (!room) {
         return;
       }
 
-      if (type === 'room.sound') {
-        spark
-          .room(room)
-          .except(spark.id)
-          .write({
-            type,
-            id,
-            ...data,
-          });
-
-        return;
-      }
+      spark.room(room).except(spark.id).write(payload);
     });
   });
 };
