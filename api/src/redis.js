dab0839fa5950a2cae62680f7657a2a425915e90
Florian Pichler <fp@ylk.gd>
2021-05-07 20:45:52 +0200

    Update redis configuration

--- api/src/redis.js
+++ api/src/redis.js
@@ -1,5 +1,9 @@
 const Redis = require('ioredis');
 
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL, {
+  tls: {
+      rejectUnauthorized: false
+  }
+});
 
 module.exports = redis;
